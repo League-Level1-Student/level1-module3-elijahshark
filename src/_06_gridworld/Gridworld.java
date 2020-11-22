9bbc87f567a7c887525405bf8e4f1eb9016ff9ca
root <root@b6df79771657>
2020-11-22 19:22:44 +0000

    yeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeet

--- src/_06_gridworld/Gridworld.java
+++ src/_06_gridworld/Gridworld.java
@@ -1,6 +1,9 @@
 package _06_gridworld;
 
+import java.util.Random;
+
 import info.gridworld.actor.Bug;
+import info.gridworld.grid.Location;
 import info.gridworld.world.World;
 
 public class Gridworld {
@@ -8,6 +11,13 @@ public class Gridworld {
 		World w = new World();
 		w.show();
 	Bug b = new Bug();
+	Location l = new Location(1,1);
+	w.add(l, b);
+	Bug b2 = new Bug();
+	Random r = new Random();
+	int randX = r.nextInt(10);
+	int randY = r.nextInt(10);
+	Location l2 = new Location(randX,randY);
 	
 	
 	
