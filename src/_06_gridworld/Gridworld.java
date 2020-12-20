616fa6e48d78f43a39a1dd464dbafdba9f323b0d
student <student@ip-172-31-54-103.us-west-2.compute.internal>
2020-12-20 11:23:30 -0800

    Yeet

--- src/_06_gridworld/Gridworld.java
+++ src/_06_gridworld/Gridworld.java
@@ -1,13 +1,16 @@
 package _06_gridworld;
 
+import java.awt.Color;
 import java.util.Random;
 
 import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
 import info.gridworld.grid.Location;
 import info.gridworld.world.World;
 
 public class Gridworld {
 	public static void main(String[] args) {
+		Flower f = new Flower();
 		World w = new World();
 		w.show();
 	Bug b = new Bug();
@@ -18,10 +21,10 @@ public class Gridworld {
 	int randX = r.nextInt(10);
 	int randY = r.nextInt(10);
 	Location l2 = new Location(randX,randY);
-	
-	
-	
-	
+	b.setColor(Color.BLUE);
+	b.turn();
+	b.turn();
+	Location flowerLoc = f.getLocation();
 	
 	
 	
